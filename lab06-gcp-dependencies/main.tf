94200d09cc759498cc0f5df93ce9323401e691c1
Srilekha2003 <srilekhakishan2003@gmail.com>
2026-06-08 14:03:04 +0530

    lab06-code-done

---
+++ lab06-gcp-dependencies/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+    }
+  }
+}
+provider "google"{
+  project = "internal-sandbox-446612"
+  region  = "asia-south1"
+}
+resource "google_storage_bucket" "bucket" {
+  name     = "srilekha-bucket-terraform"
+  location = "ASIA-SOUTH1"
+  force_destroy = true
+}
+resource "google_service_account" "sa" {
+  account_id   = "srilekha-sa"
+  display_name = "Service Account for Terraform"
+}
+resource "google_storage_bucket_iam_member" "bucket_iam" {
+  bucket = google_storage_bucket.bucket.name
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:${google_service_account.sa.email}"
+}
