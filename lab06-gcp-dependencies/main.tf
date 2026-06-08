15d0415166a993741a15792aceef56537742fb05
Srilekha2003 <srilekhakishan2003@gmail.com>
2026-06-08 08:56:17 +0000

    lab06 done

--- lab06-gcp-dependencies/main.tf
+++ lab06-gcp-dependencies/main.tf
@@ -1,17 +1,17 @@
 terraform {
   required_providers {
     google = {
-        source = "hashicorp/google"
+      source = "hashicorp/google"
     }
   }
 }
-provider "google"{
+provider "google" {
   project = "internal-sandbox-446612"
   region  = "asia-south1"
 }
 resource "google_storage_bucket" "bucket" {
-  name     = "srilekha-bucket-terraform"
-  location = "ASIA-SOUTH1"
+  name          = "srilekha-bucket-terraform"
+  location      = "ASIA-SOUTH1"
   force_destroy = true
 }
 resource "google_service_account" "sa" {
