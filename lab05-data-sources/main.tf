4b021c25294ac24d209535174fadce682b2230fa
Srilekha2003 <srilekhakishan2003@gmail.com>
2026-06-08 11:39:19 +0530

    some changes in lab05

--- lab05-data-sources/main.tf
+++ lab05-data-sources/main.tf
@@ -1,25 +1,27 @@
 terraform {
   required_providers {
     google = {
-        source = "hashicorp/google"
+      source = "hashicorp/google"
     }
   }
 }
+
 provider "google" {
   project = "internal-sandbox-446612"
 }
 
 data "google_project" "current" {
-  
+  project_id = "internal-sandbox-446612"
 }
+
 output "project_id" {
   value = data.google_project.current.project_id
 }
 
- output "project_number" {
+output "project_number" {
   value = data.google_project.current.number
- }
+}
 
- output "project_name" {
-    value = data.google_project.current.name
- }
+output "project_name" {
+  value = data.google_project.current.name
+}
